64d246256b14fa60b658d263bdaa29239e287cec
Sizoborod <se_t610@bk.ru>
2024-03-03 21:41:48 +0300

    Не проходит авторизация, печатаю некоторые параметры 5

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def login():
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
-        msg = f'{User.email} - User.email\n{form.password.data} - form.password.data\n'
+        msg = f'{user.email} - User.email\n{form.password.data} - form.password.data\n'
         a = f'___ - user.check_password(form.password.data)\n'
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
