63338e56e11567c839bc1ec61655bdf97bb60e08
Sizoborod <80516842+Sizoborod@users.noreply.github.com>
2024-05-15 12:48:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -336,8 +336,8 @@ def add_sensors():
     text += f"t(снаружи) = {request.args.get('t_out')}\n"
     text += f"Влажность(снаружи) = {request.args.get('h_out')}\n"
     
-    text += f"Влажность почвы = {int((request.args.get('mois1') + request.args.get('mois2'))/2)}\n"
-    text += f"Насос {'ВКЛ' if request.args.get('pump') else 'ВЫКЛ'}\n"
+    text += f"Влажность почвы = {int((int(request.args.get('mois1')) + int(request.args.get('mois2')))/2)}\n"
+    text += f"Насос {'ВКЛ' if int(request.args.get('pump')) else 'ВЫКЛ'}\n"
     url = f"https://api.telegram.org/bot7049728878:AAFmieYbaKdMKC9i3s96UVY4xQFtuvn7eGg/sendMessage?chat_id=6556861081&text={text}"
     r = requests.get(url)
     r.status_code
